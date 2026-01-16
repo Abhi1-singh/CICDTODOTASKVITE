d1a5b3a339c9596f195d93f8eb10288b0d0a46f6
abhisheksingh@aviansoft.in <abhisheksinghaviansoft@gmail.com>
2026-01-17 00:03:14 +0530

    firstsss commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,39 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://cicdtodotask.onrender.com/api"
+// });
+
+// export default api;
+
+
+// src/api/api.js
 // src/api/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cicdtodotask.onrender.com/api"
+  baseURL: "https://cicdtodotask.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
+
+
+
+// import axios from "axios";
+
+// const isLocalhost =
+//   window.location.hostname === "localhost";
+
+// const api = axios.create({
+//   baseURL: isLocalhost
+//     ? "http://localhost:3000/api"
+//     : "https://cicdtodotask.onrender.com/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default api;
