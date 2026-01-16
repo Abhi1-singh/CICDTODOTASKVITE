97f3193653c62df01702528f722a137e48432cb9
abhisheksingh@aviansoft.in <abhisheksinghaviansoft@gmail.com>
2026-01-17 00:15:40 +0530

    firstssss commit

--- src/api/api.js
+++ src/api/api.js
@@ -9,31 +9,36 @@
 
 // src/api/api.js
 // src/api/api.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://cicdtodotask.onrender.com/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-export default api;
-
-
-
 // import axios from "axios";
 
-// const isLocalhost =
-//   window.location.hostname === "localhost";
-
 // const api = axios.create({
-//   baseURL: isLocalhost
-//     ? "http://localhost:3000/api"
-//     : "https://cicdtodotask.onrender.com/api",
+//   baseURL: "https://cicdtodotask.onrender.com/api",
 //   headers: {
 //     "Content-Type": "application/json",
 //   },
 // });
 
 // export default api;
+
+
+import express from "express";
+import cors from "cors";
+import todoRoutes from "./routes/todo.routes.js";
+
+const app = express();
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://cicdtodotaskvite.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.use(express.json());
+
+app.get("/health", (req, res) => res.send("OK"));
+app.use("/api/todos", todoRoutes);
+
+export default app;
